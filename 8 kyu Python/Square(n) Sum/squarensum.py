7d2352b9528267e8edb81f15d593d43994557e0b
sebmasta <sebmasta@gmail.com>
2024-11-25 22:11:21 +0100

    squarensum solution

---
+++ 8 kyu Python/Square(n) Sum/squarensum.py
@@ -0,0 +1,17 @@
+#codewars solution
+'''
+def square_sum(numbers):
+    #your code here
+    x = 0
+    for number in numbers:
+        x = x + number ** 2
+        print(x)
+    return x
+'''
+#tested code 
+a = [1, 2, 2]
+x = 0
+for number in a:
+    x = x + number ** 2
+    print(x)
+print(x)
